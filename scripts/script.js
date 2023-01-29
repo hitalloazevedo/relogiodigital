8d0ed951cb74951e7606e0080bf2b2389a34acbf
HitalloAzevedo <azevedohitallo@gmail.com>
2023-01-29 19:16:52 -0300

    mudando a estrutura
    mudando o projeto de html para javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,28 @@
-function atualizar_horas(){
+window.onload = () => {
+    relogio.appendChild(display);
+    relogio.appendChild(footer);
+}
+
+
+const relogio = document.querySelector(".relogio");
+
+//cria uma div e aciciona a classe display
+const display = document.createElement("div");
+display.classList.add("display"); 
+
+//cria a tag footer
+const footer = document.createElement("footer");
+
+//Cria o link do github e adiciona na tag footer 
+const myLink = '<a href="https://github.com/hitalloazevedo" target="_blank">Hitallo Azevedo</a>'
+footer.innerHTML = "Criado por " + myLink;
+
+
+const atualizar_horas = () => {
     var data = new Date();
-    var display = document.querySelector(".display")
 
     var horario = corrigir_horario(data.getHours()) + ":" + corrigir_horario(data.getMinutes()) + ":" +corrigir_horario(data.getSeconds());
+
     display.textContent = horario
 }
 
